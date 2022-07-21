2ab2a6cfc5b935cc46ea81b90050c9b3fae73677
Maestro <sididagnon@gmail.com>
2022-07-21 16:55:37 +0000

    Commit du 21 Juillet 2022

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    name2 = models.fields.CharField(max_length=100)
 
 class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
