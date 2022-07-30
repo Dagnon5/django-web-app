3ea1cf9cf697ca37fd6bf51522f723615370c72e
Maestro <sididagnon@gmail.com>
2022-07-30 12:16:07 +0000

    Partiel R du CRUD

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 from listings.models import  Band
 from listings.models import Listing
-from django.shortcuts import  render
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return  render(request, 'listings/hello.html' ,
+    return  render(request, 'listings/band_list.html' ,
                    {'bands':bands})
+def band_detail(request,id):
+    band = Band.objects.get(id=id) # Récupérer le band avec cet id
+    return render(request, 'listings/band_detail.html',
+                  {'band':band}) # Pour passer le groupe au gabarit
 
 def listings(request):
     listes = Listing.objects.all()
     return render(request, 'listes_title/listings.html', {'listes':listes})
+
+
+def listing_detail(request, id):
+    #listes_det = Listing.objects.get(id=id)
+    return render(request, 'listings/listing_detail.html',
+                   {'id':id})
+
 
 def about(request):
     return  render(request, 'about/about.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -19,8 +19,12 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    #Chemin Bands
+    path('bands/', views.band_list, name ='band-list'),
+    path('bands/<int:id>/',views.band_detail, name = 'band-detail'),
     path('about-us/', views.about),
-    path('listings/', views.listings),
+    # Chemin Listings
+    path('listings/', views.listings, name='listing-list'),
+    path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
     path('contact-us/',views.contact),
 ]
